c3d6da7ba80aa911deafd321672955f3083e5a3a
João Vinícius <joaoviniciusferdlbqt@gmail.com>
2025-01-02 22:32:29 -0300

    Preenchendo dropdown list select com JQuery

--- routes/autores.js
+++ routes/autores.js
@@ -6,7 +6,7 @@ router.get('/listar', function(req, res) {
   let cmd = ' SELECT IdAutor, NoAutor, NoNacionalidade';
       cmd += ' FROM TbAutor AS a INNER JOIN TbNacionalidade AS n';
       cmd += ' ON a.IdNacionalidade = n.IdNacionalidade';
-      cmd += ' ORDER BY NoAutor';
+      cmd += ' ORDER BY NoNacionalidade';
 
   db.query(cmd, [], function(erro, listagem){
     if (erro){router.get('/', function(req, res, next) {
@@ -15,6 +15,20 @@ router.get('/listar', function(req, res) {
       res.send(erro);
     }
     res.render('autores-lista', {resultado: listagem});
+  });
+});
+
+router.post('/add', function(req, res) {
+  let nome = req.body.nome;
+  let nacionalidade = req.body.nacionalidade;
+  let cmd = ' INSERT INTO TbAutor (NoAutor, IdNacionalidade) VALUES (?, ?);';
+  db.query(cmd, [nome, nacionalidade], function(erro, listagem){
+    if (erro){router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Cafeteria' });
+});
+      res.send(erro);
+    }
+    res.redirect('/autores/listar');
   });
 });
 
